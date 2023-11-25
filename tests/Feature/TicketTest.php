791320fe8d31ebc4c0d325063a21b96cb4e3b290
Joao Vitor Lima Conceição <joao.conceicao@ses.ba>
2023-11-25 15:38:23 -0300

    Removendo validacao de cpf duplicado na tabela de passageiro

--- tests/Feature/TicketTest.php
+++ tests/Feature/TicketTest.php
@@ -104,6 +104,52 @@ class TicketTest extends TestCase
         $response->assertStatus(201);
     }
 
+    public function testIfTicketCanBeCreatedWitHManyPassengersAndBaggages()
+    {
+        $seat = Seat::factory()
+            ->for(FlightClass::factory()
+                ->for(FlightClassType::factory()->create(['name' => 'Primeira classe']))
+                ->for(Flight::factory()
+                    ->for(Airport::factory()->create(), 'flightOriginAirport')
+                    ->for(Airport::factory()->create(), 'flightDestinationAirport')
+                    ->create()))
+            ->count(3)->create();
+
+        $requestData = [
+            'purchaser' => [
+                'name' => 'Victor Teste',
+                'email' => 'teste@gmail.com',
+                'cpf' => '672.382.690-66',
+                'birth_date' => '25/06/1994',
+            ],
+            'qty_tickets' => 3,
+            'has_baggage_exceeded' => true,
+            'seat_id' => $seat->toArray()[0]['id'],
+            'passengers' => [
+                [
+                    'name' => 'Fernanda Lima',
+                    'email' => 'fernandalima@gmail.com',
+                    'cpf' => '547.386.150-41',
+                    'birth_date' => '25/06/1994',
+                    'has_baggage_exceeded' => true,
+                    'seat_id' => $seat->toArray()[1]['id'],
+                ],
+                [
+                    'name' => 'Ricardo Silva',
+                    'email' => 'ricardosilva@gmail.com',
+                    'cpf' => '547.386.150-41',
+                    'birth_date' => '25/06/1994',
+                    'has_baggage_exceeded' => true,
+                    'seat_id' => $seat->toArray()[2]['id'],
+                ],
+            ]
+        ];
+
+        $response = $this->post('/api/tickets', $requestData);
+        $response->assertSee('Registro inserido com sucesso');
+        $response->assertStatus(201);
+    }
+
 
     public function testIfTicketCanBeDeleted()
     {
